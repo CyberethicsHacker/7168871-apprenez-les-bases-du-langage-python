6281e5a6b506b646394b5f88f30167ec48d93ef6
CyberethicsHacker <avatarchange2@gmail.com>
2024-06-20 11:18:33 -0400

    Update main.py
    test1

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,6 @@
-# Écrivez votre code ici !
+# creation variable
+nom="andre"
+age=10
+print (f"bonjour,je m'appelle{nom} et j'ai {age} ans.")
+age=age+10
+print (f"bonjour,je m'appelle{nom} et j'ai {age} ans.")
